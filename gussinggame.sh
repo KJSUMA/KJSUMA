7e7e28aad406e9004e860d72b4e3249d3d1a2c85
KJSUMA <147533910+KJSUMA@users.noreply.github.com>
2023-10-10 21:31:53 +0530

    Update gussinggame.sh

--- gussinggame.sh
+++ gussinggame.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+# Function to count the number of files in the current directory
+count_files() {
+    local count=$(ls -l | grep -v ^l | wc -l)
+    echo "$count"
+}
+
+# Main logic
+actual_count=$(count_files)
+
+while true; do
+    echo "Guess the number of files in the current directory:"
+    read user_guess
+
+    if [[ $user_guess -lt $actual_count ]]; then
+        echo "Too low! Try again."
+    elif [[ $user_guess -gt $actual_count ]]; then
+        echo "Too high! Try again."
+    else
+        echo "Congratulations! You guessed correctly."
+        break
+    fi
+done
 
